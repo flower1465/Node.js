fe4e1562207484439c37b8f9b9cb0b0718813fd1
Seung jun <poodle1465@daum.net>
2020-12-02 19:30:13 +0900

    Self_Study
    socket_learn

--- Self-Study/WS/app.js
+++ Self-Study/WS/app.js
@@ -1,30 +1,57 @@
 const express = require("express");
+const path = require("path");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
-const { sequelize } = require("./models");
-const routes = require("./routes");
 
 dotenv.config();
+const webSocket = require("./socket");
+const indexRouter = require("./routes");
 
 const app = express();
+app.set("port", process.env.PORT || 8005);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
 
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-const PORT = process.env.PORT || 3002;
-
-sequelize
-  .sync({ force: true })
-  .then(() => {
-    console.log("데이터베이스 연결 성공");
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
   })
-  .catch((err) => {
-    console.error(err);
-  });
+);
 
-app.use("/", routes);
+app.use("/", indexRouter);
 
-app.listen(PORT, () => {
-  console.log(PORT, "번 포트에서 대기 중");
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+const server = app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
+});
+
+webSocket(server);
